be7022f46cb7e80864fd010ceb953fa1259b1396
Joe <51638822+tso996@users.noreply.github.com>
2022-07-19 12:07:34 +0100

    tri appears

--- main.cpp
+++ main.cpp
@@ -10,21 +10,49 @@
 
 
 
-static unsigned int compileShader(unsigned int type, const std::string& source){
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();//&source[0]
-    
-    
-    return 1;
-}
 
-static int createShader(const std::string& vertexShader, const std::string& fragmentShader){
-    
-    unsigned int program = glCreateProgram();
-    unsigned int v = compileShader(GL_VERTEX_SHADER, vertexShader);
-    
-    return 1;
-}
+ static void CheckStatus( GLuint obj )
+    {
+        GLint status = GL_FALSE;
+        if( glIsShader(obj) ) glGetShaderiv( obj, GL_COMPILE_STATUS, &status );
+        if( glIsProgram(obj) ) glGetProgramiv( obj, GL_LINK_STATUS, &status );
+        if( status == GL_TRUE ) return;
+        GLchar log[ 1 << 15 ] = { 0 };
+        if( glIsShader(obj) ) glGetShaderInfoLog( obj, sizeof(log), NULL, log );
+        if( glIsProgram(obj) ) glGetProgramInfoLog( obj, sizeof(log), NULL, log );
+        std::cerr << log << std::endl;
+        std::exit( EXIT_FAILURE );
+    }
+
+    static void AttachShader( GLuint program, GLenum type, const char* src )
+    {
+        GLuint shader = glCreateShader( type );
+        glShaderSource( shader, 1, &src, NULL );
+        glCompileShader( shader );
+        CheckStatus( shader );
+        glAttachShader( program, shader );
+        glDeleteShader( shader );
+    }
+
+    static GLuint Load( const char* shader, ... )
+    {
+        GLuint prog = glCreateProgram();
+        va_list args;
+        va_start( args, shader );
+        while( shader )
+        {
+            const GLenum type = va_arg( args, GLenum );
+            AttachShader( prog, type, shader );
+            shader = va_arg( args, const char* );
+        }
+        va_end( args );
+        glLinkProgram( prog );
+        CheckStatus( prog );
+        return prog;
+    }
+
+
+
 
 
 
@@ -45,8 +73,8 @@ int main(int argc, const char * argv[]) {
     }
     
     //The following is required to change the opengl version from 2.1 to 4.1 in macs..
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     //===============================================================
@@ -73,9 +101,17 @@ int main(int argc, const char * argv[]) {
     
     std::cout<<"gl version: "<<glGetString(GL_VERSION)<<std::endl;
     
-    float positions[6] ={-0.5f, -0.5f,
-                          0.0f,0.5f,
-                          0.5f, -0.5f};
+    GLuint vao = 0;
+    glGenVertexArrays( 1, &vao );
+    glBindVertexArray( vao );
+
+
+    float positions[ 6 ] =
+    {
+        -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f, -0.5f,
+    };
     
     unsigned int buffer;//variable to store the id of the buffer
     glGenBuffers(1/*could be more than one*/, &buffer);//creates a buffer and returns the id of the buffer..
@@ -84,9 +120,24 @@ int main(int argc, const char * argv[]) {
     //At this point the gpu just knows we have provided some data, it does not know the type of data, number of bytes of data, or that 2 elements from the positions array actually represent one point/vertex of the triangle..
     
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);//tell the gpu details about the bytes it has in the buffer with id
-    glEnableVertexAttribArray(buffer);//enable the vertex
+    std::cout<<"buffer id: "<<buffer<<std::endl;
+    glEnableVertexAttribArray(0);//enable the vertex
 
-    
+    std::string vertexShader =
+        "#version 410 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main(){\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 410 core\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main(){\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+    unsigned int shader = Load( vertexShader.c_str(), GL_VERTEX_SHADER, fragmentShader.c_str(), GL_FRAGMENT_SHADER, NULL );
+    glUseProgram( shader );
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
